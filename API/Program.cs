eb9da1ebb690add2f3223e464d4bd029e85ccf98
Brunno Roberto <brunnophoenix@gmail.com>
2024-10-01 18:45:04 -0300

    Update Deploy Application remove UseUrls no builder

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,6 @@ static IHostBuilder CreateHostBuilder(string [] args)
     return Host.CreateDefaultBuilder(args)
         .ConfigureWebHost(webBuilder => 
         {
-            webBuilder.UseUrls("http://0.0.0.0:5223");
             webBuilder.UseKestrel();
             webBuilder.UseStartup<Startup>();
         });
