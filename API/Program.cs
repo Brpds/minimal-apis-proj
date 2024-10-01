d860159e1df2d5fdabfdfe94d7e2ab2e53613f98
Brunno Roberto <brunnophoenix@gmail.com>
2024-10-01 18:39:56 -0300

    Update Deploy Application Adds UseUrls no builder

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ static IHostBuilder CreateHostBuilder(string [] args)
     return Host.CreateDefaultBuilder(args)
         .ConfigureWebHost(webBuilder => 
         {
+            webBuilder.UseUrls("http://0.0.0.0:5223");
             webBuilder.UseKestrel();
             webBuilder.UseStartup<Startup>();
         });
